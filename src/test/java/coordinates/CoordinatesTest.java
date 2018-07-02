1c8d43dc64f73e50e172cbfbfb9a38afe779a5d3
Maren Kruse <mkruse@thoughtworks.com>
2018-07-02 15:09:35 +0200

    rename coordinates test methods and extend marsRover unit tests

--- src/test/java/coordinates/CoordinatesTest.java
+++ src/test/java/coordinates/CoordinatesTest.java
@@ -10,33 +10,33 @@ public class CoordinatesTest {
     private Coordinates coordinates;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         coordinates = new Coordinates(3,4);
     }
 
     @Test
-    public void increaseXByOne() {
+    public void shouldIncreaseOnlyXCoordinateByOne() {
         coordinates.increaseXByOne();
         assertEquals(4, coordinates.getX());
         assertEquals(4, coordinates.getY());
     }
 
     @Test
-    public void increaseYByOne() {
+    public void shouldIncreaseOnlyYCoordinateByOne() {
         coordinates.increaseYByOne();
         assertEquals(3, coordinates.getX());
         assertEquals(5, coordinates.getY());
     }
 
     @Test
-    public void decreaseXByOne() {
+    public void shouldDecreaseOnlyXCoordinateByOne() {
         coordinates.decreaseXByOne();
         assertEquals(2, coordinates.getX());
         assertEquals(4, coordinates.getY());
     }
 
     @Test
-    public void decreaseYByOne() {
+    public void shouldDecreaseOnlyYCoordinateByOne() {
         coordinates.decreaseYByOne();
         assertEquals(3, coordinates.getX());
         assertEquals(3, coordinates.getY());
--- src/test/java/rovers/MarsRoverTest.java
+++ src/test/java/rovers/MarsRoverTest.java
@@ -14,28 +14,38 @@ public class MarsRoverTest {
     private MarsRover rover;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         coordinates = new Coordinates(1,2);
         direction = Direction.NORTH;
+        rover = new MarsRover(coordinates, direction);
     }
 
     @Test
-    public void shouldChangeDirectionToTheRight_WhenRoverTurnsToRightSide() {
-        rover = new MarsRover(coordinates, direction);
+    public void shouldChangeDirectionClockwise_WhenRoverTurnsToRightSide() {
         rover.turnRight();
         assertEquals(Direction.EAST, rover.getDirection());
+        rover.turnRight();
+        assertEquals(Direction.SOUTH, rover.getDirection());
+        rover.turnRight();
+        assertEquals(Direction.WEST, rover.getDirection());
+        rover.turnRight();
+        assertEquals(Direction.NORTH, rover.getDirection());
     }
 
     @Test
-    public void shouldChangeDirectionToTheLeft_WhenRoverTurnsToLeftSide() {
-        rover = new MarsRover(coordinates, direction);
+    public void shouldChangeDirectionAnticlockwise_WhenRoverTurnsToLeftSide() {
         rover.turnLeft();
         assertEquals(Direction.WEST, rover.getDirection());
+        rover.turnLeft();
+        assertEquals(Direction.SOUTH, rover.getDirection());
+        rover.turnLeft();
+        assertEquals(Direction.EAST, rover.getDirection());
+        rover.turnLeft();
+        assertEquals(Direction.NORTH, rover.getDirection());
     }
 
     @Test
     public void shouldIncreaseXCoordinateByOneStep_WhenRoverMovesOneStepForward() {
-        rover = new MarsRover(coordinates, direction);
         rover.moveForward();
         assertEquals(1, rover.getCoordinates().getX());
         assertEquals(3, rover.getCoordinates().getY());
