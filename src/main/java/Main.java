e6862527e5b16a12abab25d7675475b50dfd1a8e
Maren Kruse <mkruse@thoughtworks.com>
2018-07-10 12:37:21 +0200

    add rover controller as orchestrator

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,40 @@
-import commands.CommandParser;
+import commands.*;
 import coordinates.Coordinates;
 import directions.Direction;
+import plateaus.MarsPlateau;
 import rovers.MarsRover;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        CommandParser parser = new CommandParser();
-        parser.readInput("input.txt");
+        //CommandParser parser = new CommandParser();
+        //parser.readInput("input.txt");
 
-        Direction direction = Direction.NORTH;
-        Coordinates coordinates = new Coordinates(1,2);
-        MarsRover firstRover = new MarsRover(coordinates, direction);
-        firstRover.turnRight();
-        firstRover.moveForward();
+        //expected output from parser for plateau initialization
+        int [] plateauDimensions = new int[] {5, 5};
+        int upperXPlateau = plateauDimensions[0];
+        int upperYPlateau = plateauDimensions[1];
 
-        System.out.println(firstRover.printCurrentLocation());
+        //expected output from parser for Rover initialization? where to all the parsing, eg from char to enum?
+        int xRover = 1;
+        int yRover = 2;
+        Direction initialDirection = Direction.NORTH;
 
-        firstRover.turnLeft();
+        //expected output from parser for plateau initialization
+        String commandString = "LMLMLMLMM";
 
-        System.out.println(firstRover.printCurrentLocation());
+        RoverController controller = new RoverController();
+
+        MarsPlateau plateau = controller.initPlateau(upperXPlateau, upperYPlateau);
+        Coordinates initialCoordinates = controller.initCoordinates(xRover, yRover);
+        MarsRover rover = controller.initRover(initialCoordinates, initialDirection, plateau);
+        List<Command> commandSequence = controller.setCommandSequence(commandString, rover);
+        controller.runCommands(commandSequence);
+
+        System.out.println(rover.printCurrentLocation());
     }
 }
--- src/main/java/commands/RoverController.java
+++ src/main/java/commands/RoverController.java
@@ -1,13 +1,54 @@
 package commands;
 
+import coordinates.Coordinates;
+import directions.Direction;
+import plateaus.MarsPlateau;
+import rovers.MarsRover;
+
+import java.util.LinkedList;
+import java.util.List;
+
 public class RoverController {
 
-    Command command;
+    public MarsPlateau initPlateau(int upperX, int upperY) {
+        return MarsPlateau.getPlateau(upperX, upperY);
+    }
 
-    public void setCommands(Command command){
-       this.command = command;
+    public Coordinates initCoordinates(int x, int y) {
+        return new Coordinates(x, y);
     }
-    public void runCommands(){
-        command.execute();
+
+    public MarsRover initRover(Coordinates initCoordinates, Direction initDirection, MarsPlateau plateau) {
+        return new MarsRover(initCoordinates, initDirection, plateau);
     }
+
+    public List<Command> setCommandSequence(String commandString, MarsRover rover) {
+        List<Command> commandSequence = new LinkedList<>();
+        commandString.chars().forEach(c -> {
+            switch (c) {
+                case ('R'):
+                    commandSequence.add(new TurnRightCommand(rover));
+                    break;
+                case ('L'):
+                    commandSequence.add(new TurnLeftCommand(rover));
+                    break;
+                case ('M'):
+                    commandSequence.add(new MoveCommand(rover));
+                    break;
+                default:
+                    throw new IllegalArgumentException(
+                            "Illegal Argument when setting command sequence for rover");
+            }
+        });
+        return commandSequence;
+    }
+
+    public void runCommands(List<Command> commandSequence) {
+        commandSequence.forEach(Command::execute);
+    }
+
 }
+
+
+
+
